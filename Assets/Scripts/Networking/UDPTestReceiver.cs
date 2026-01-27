d9c80437ac9fb5bee45ac38abb2be9c8497672c0
Mike Molekamp <mbc.molekamp@student.avans.nl>
2026-01-27 12:30:17 +0100

    Added UDPServer still untested

--- Assets/Scripts/Networking/UDPTestReceiver.cs
+++ Assets/Scripts/Networking/UDPTestReceiver.cs
@@ -1,19 +1,49 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 
 namespace DefqonEngine
 {
     public class UDPTestReceiver : MonoBehaviour
     {
+        public bool start = false;
+        public int listenPort = 1111;
+        public UdpClient udpServer;
+        public IPEndPoint remoteEP;
+
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            // Initialize server
+            udpServer = new UdpClient(listenPort);
+            remoteEP = new IPEndPoint(IPAddress.Any, 0);
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            try
+            {
+                // Wait for message
+                byte[] receivedMessage = udpServer.Receive(ref remoteEP);
+
+                // Convert byte[] to readable string
+                string message = Encoding.UTF8.GetString(receivedMessage);
+
+                // Check if its a start message
+                if (message != null && message == "Start");
+                {
+                    start = true;
+                    Console.WriteLine("Received start message");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
